b930b9abecd3774479497ba6697311e7c1d8cc27
Rajat20 <30955711+Rajat20@users.noreply.github.com>
2017-08-19 22:49:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 //change the text the main text
-    var element = Document.getElementByID("main-text");
+    var element = Document.getElementByID('main-text');
     element.innerHTML='new value';
     
 //to move image
